c8e030b394e3b2139d99879cc3ba3d138c08c360
Joshua E Caponong <Joshua.Caponong@ibm.com>
2022-05-06 19:49:27 +0800

    initial commit

---
+++ src/main/java/com/caponong/transactionreconciliator/TransactionReconciliatorApplication.java
@@ -0,0 +1,13 @@
+package com.caponong.transactionreconciliator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionReconciliatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionReconciliatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caponong/transactionreconciliator/TransactionReconciliatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caponong.transactionreconciliator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionReconciliatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
